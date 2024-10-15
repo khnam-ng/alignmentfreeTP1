6bf4eeb475ce5a35bb383e2f8e47595384a27497
smivys <khanhnamxz@hotmail.com>
2024-10-15 12:22:33 +0200

    Rendu_1 correction (stole it)

--- TP/__main__.py
+++ TP/__main__.py
@@ -1,59 +1,110 @@
 from TP.loading import load_directory
-from TP.kmers import stream_kmers, kmer2str, enumerate_kmer
+from TP.kmers import stream_kmers, kmer2str
+
 import os
 
+def jaccard_from_sorted_lists(lstA, lstB):
+    idxA = 0
+    idxB = 0
 
+    intersection = 0
+    union = 0
 
-def jaccard(fileA, fileB, k):
-    # j = 0
-    # --- To complete ---
-    index = stream_kmers(fileA, k)
-    intersect = 0
-    union = sum(index.values())
-    for kmer in enumerate_kmer(fileB, k):
-        if kmer in index and index[kmer] > 0:
-            intersect += 1
-            index[kmer] -= 1
-    else:
+    while idxA < len(lstA) and idxB < len(lstB):
         union += 1
+        if lstA[idxA] == lstB[idxB]:
+            intersection += 1
+            idxA += 1
+            idxB += 1
+        elif lstA[idxA] < lstB[idxB]:
+            idxA += 1
+        else:
+            idxB += 1
 
-    return round(intersect/union, 3)
-    # return j
+    union += len(lstA) - idxA
+    union += len(lstB) - idxB
+
+    return intersection / union
+
 
 
 if __name__ == "__main__":
     print("Computation of Jaccard similarity between files")
 
     # Load all the files in a dictionary
+    print("Loading files")
     files = load_directory("data")
     k = 21
-            
-    print("Computing Jaccard similarity for all pairs of samples")
+    
     filenames = list(files.keys())
-    matrix = [list(range(0, len(filenames)+1))]
-    for i in range(1, len(filenames)+1):
-        matrix.append(list(range(i, len(filenames)+1+i)))
     
+    # Create all the kmer lists (can be expensive in memory)
+    print("Computing all kmer vectors")
+    kmer_lists = {}
+    for filename in filenames:
+        kmer_lists[filename] = []
+        # Enumerate all the sequences from a fasta
+        for seq in files[filename]:
+            kmer_lists[filename].extend(stream_kmers(seq, k))
+        # Sort the kmer lists to speed up the comparison
+        kmer_lists[filename].sort()
+
+    print("Computing Jaccard similarity for all pairs of samples")
     for i in range(len(files)):
-        matrix[0][i+1] = filenames[i]
-        matrix[i+1][0] = filenames[i]
-        for j in range(len(files)):
-            # --- Complete here ---
-            if i == j:
-                matrix[i+1][j+1] = 0
-            else:
-                J = jaccard(files[filenames[i]][0], files[filenames[j]][0], k)
-                print(filenames[i], filenames[j], J)
-                matrix[j+1][i+1] = J
+        for j in range(i+1, len(files)):
+            jaccard = jaccard_from_sorted_lists(kmer_lists[filenames[i]], kmer_lists[filenames[j]])
+            print(filenames[i], filenames[j], jaccard)
+
+# def jaccard(fileA, fileB, k):
+#     # j = 0
+#     # --- To complete ---
+#     index = stream_kmers(fileA, k)
+#     intersect = 0
+#     union = sum(index.values())
+#     for kmer in enumerate_kmer(fileB, k):
+#         if kmer in index and index[kmer] > 0:
+#             intersect += 1
+#             index[kmer] -= 1
+#     else:
+#         union += 1
+
+#     return round(intersect/union, 3)
+#     # return j
+
+
+# if __name__ == "__main__":
+#     print("Computation of Jaccard similarity between files")
+
+#     # Load all the files in a dictionary
+#     files = load_directory("data")
+#     k = 21
+            
+#     print("Computing Jaccard similarity for all pairs of samples")
+#     filenames = list(files.keys())
+#     matrix = [list(range(0, len(filenames)+1))]
+#     for i in range(1, len(filenames)+1):
+#         matrix.append(list(range(i, len(filenames)+1+i)))
     
-    print('Distance matrix: \n',matrix)
+#     for i in range(len(files)):
+#         matrix[0][i+1] = filenames[i]
+#         matrix[i+1][0] = filenames[i]
+#         for j in range(len(files)):
+#             # --- Complete here ---
+#             if i == j:
+#                 matrix[i+1][j+1] = 0
+#             else:
+#                 J = jaccard(files[filenames[i]][0], files[filenames[j]][0], k)
+#                 print(filenames[i], filenames[j], J)
+#                 matrix[j+1][i+1] = J
+    
+#     print('Distance matrix: \n',matrix)
 
  
-    file_path = 'distance_matrix.txt'
-    if not os.path.exists(file_path):
-        with open(file_path, 'w') as file:
-            for row in matrix:
-                file.write(str(row))
-                file.write('\n')
-    else:
-        print(f"The file {file_path} already exists.")
+#     file_path = 'distance_matrix.txt'
+#     if not os.path.exists(file_path):
+#         with open(file_path, 'w') as file:
+#             for row in matrix:
+#                 file.write(str(row))
+#                 file.write('\n')
+#     else:
+#         print(f"The file {file_path} already exists.")
--- TP/kmers.py
+++ TP/kmers.py
@@ -1,27 +1,3 @@
-from collections import defaultdict
-
-def encode_nucl(letter):
-    encoding = {'A': 0, 'C': 1, 'T': 2, 'G': 3}
-    return encoding[letter]
-
-def encode_kmer(seq, k):
-    kmer = 0
-    for letter in seq[0:k]:
-        kmer <<= 2
-        kmer += encode_nucl(letter)
-    return kmer
-
-def enumerate_kmer(seq, k):
-    kmer = encode_kmer(seq, k) #seq[0:k]
-    mask = (1 << (2*(k-1))) - 1
-    for i in range(len(seq) - k + 1):
-        yield kmer
-        kmer &= mask # kmer = kmer[1:] #
-        kmer <<= 2 # kmer.append(seq[i + k])
-        kmer |= encode_nucl(seq[i + k - 1])
-    yield kmer
-
-
 def kmer2str(val, k):
     """ Transform a kmer integer into a its string representation
     :param int val: An integer representation of a kmer
@@ -37,9 +13,89 @@ def kmer2str(val, k):
     str_val.reverse()
     return "".join(str_val)
 
-def stream_kmers(text, k):
-    # --- To complete ---
-    res = defaultdict(int)
-    for i in range(len(text)-k+1):
-        res[encode_kmer(text[i:i+k], k)] += 1
-    return res
+
+def encode_nucl(nucl):
+    """ Encode a nucleotide into a 2-bit integer
+    :param str nucl: The nucleotide to encode
+    :return (int, int): The encoded nucleotide and its reverse complement
+    """
+    encoded = (ord(nucl) >> 1) & 0b11 # Extract the two bits of the ascii code that represent the nucleotide
+    rencoded = (encoded + 2) & 0b11 # Complement encoding with bit tricks. Avoid slow if statement.
+
+    return encoded, rencoded
+
+
+def stream_kmers(seq, k):
+    # Initialize the kmer and its reverse complement
+    kmer = 0
+    rkmer = 0
+
+    # Add the first k-1 nucleotides to the first kmer and its reverse complement
+    for i in range(k-1):
+        nucl, rnucl = encode_nucl(seq[i])
+        kmer |= nucl << (2*(k-2-i))
+        rkmer |= rnucl << (2*(i+1))
+
+    mask = (1 << (2*(k-1))) - 1
+
+    # Yield the kmers
+    for i in range(k-1, len(seq)):
+        nucl, rnucl = encode_nucl(seq[i])
+        # Remove the leftmost nucleotide from the kmer 
+        kmer &= mask
+        # Shift the kmer to make space for the new nucleotide
+        kmer <<= 2
+        # Add the new nucleotide to the kmer
+        kmer |= nucl
+        # Make space for the new nucleotide in the reverse kmer (remove the rightmost nucleotide by side effect)
+        rkmer >>= 2
+        # Add the new nucleotide to the reverse kmer
+        rkmer |= rnucl << (2*(k-1))
+
+        yield min(kmer, rkmer)
+        
+# from collections import defaultdict
+
+# def encode_nucl(letter):
+#     encoding = {'A': 0, 'C': 1, 'T': 2, 'G': 3}
+#     return encoding[letter]
+
+# def encode_kmer(seq, k):
+#     kmer = 0
+#     for letter in seq[0:k]:
+#         kmer <<= 2
+#         kmer += encode_nucl(letter)
+#     return kmer
+
+# def enumerate_kmer(seq, k):
+#     kmer = encode_kmer(seq, k) #seq[0:k]
+#     mask = (1 << (2*(k-1))) - 1
+#     for i in range(len(seq) - k + 1):
+#         yield kmer
+#         kmer &= mask # kmer = kmer[1:] #
+#         kmer <<= 2 # kmer.append(seq[i + k])
+#         kmer |= encode_nucl(seq[i + k - 1])
+#     yield kmer
+
+
+# def kmer2str(val, k):
+#     """ Transform a kmer integer into a its string representation
+#     :param int val: An integer representation of a kmer
+#     :param int k: The number of nucleotides involved into the kmer.
+#     :return str: The kmer string formatted
+#     """
+#     letters = ['A', 'C', 'T', 'G']
+#     str_val = []
+#     for _ in range(k):
+#         str_val.append(letters[val & 0b11])
+#         val >>= 2
+
+#     str_val.reverse()
+#     return "".join(str_val)
+
+# def stream_kmers(text, k):
+#     # --- To complete ---
+#     res = defaultdict(int)
+#     for i in range(len(text)-k+1):
+#         res[encode_kmer(text[i:i+k], k)] += 1
+#     return res
